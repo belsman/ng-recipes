fb7d5722ca21503e4e92801839718d05ed45d8a9
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-10 16:22:50 +0100

    Display lists of ingredients

--- src/app/features/shopping/shopping-list/shopping-list.component.ts
+++ src/app/features/shopping/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    { name: 'apples', amount: 5 },
+    { name: 'tomotoes', amount: 10 },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

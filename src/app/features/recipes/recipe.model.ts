0a42f427699c523e7f3887decc08961565ac1ff6
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-10 17:09:29 +0100

    Add shopping-edit form view

--- src/app/features/recipes/recipe.model.ts
+++ src/app/features/recipes/recipe.model.ts
@@ -11,4 +11,10 @@ export const recipes: Recipe[] = [
     imageURL:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv7QUgLIlKoGmNlvoOQ-g-J0kTeV2efXgPVw&usqp=CAU',
   },
+  {
+    name: 'great recipe',
+    description: 'A description of the greatest recipe in the world!',
+    imageURL:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv7QUgLIlKoGmNlvoOQ-g-J0kTeV2efXgPVw&usqp=CAU',
+  },
 ];

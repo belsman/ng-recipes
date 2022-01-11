a931d6a16cbac98308e486d5229c9af6ae17e2d2
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-11 16:07:43 +0100

    Add dynamic active indicator to the nav item

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   @Output() headerItemSelected = new EventEmitter<string>();
+  selectedNav = 'recipe';
   collapsed = true;
+
+  onSelectNav(navLabel: string) {
+    this.headerItemSelected.emit(navLabel);
+    this.selectedNav = navLabel;
+  }
 }

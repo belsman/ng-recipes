ac36eb97bd4b7d42f43c6df4b69d69b42b097a27
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-11 16:46:16 +0100

    Enable adding ingredient via form

--- src/app/features/shopping/shopping-edit/shopping-edit.component.ts
+++ src/app/features/shopping/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAdd() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+
+    this.ingredientAdded.emit({ name, amount });
   }
-
 }

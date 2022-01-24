9dc523b321e226e4e69f1ea91a6fd6f8797f875c
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-25 00:39:06 +0100

    Programmatically navigate to the new route on button click

--- src/app/features/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/features/recipes/recipe-edit/recipe-edit.component.ts
@@ -8,12 +8,12 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
-  isEdit = false;
+  isEditMode = false;
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       const id = +params['id'];
-      this.isEdit = Boolean(id);
+      this.isEditMode = Boolean(id);
     });
   }
 }
--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 
 import { Recipe } from '../recipe.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,9 +13,13 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private router: Router) {}
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['recipes', 'new']);
   }
 }

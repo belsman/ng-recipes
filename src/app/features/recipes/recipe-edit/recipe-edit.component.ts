cc2a3f42b2f4f3d63789f7077cddfd5d2dccca4f
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-25 00:34:11 +0100

    Track edit and new state in recipe-edit component

--- src/app/features/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/features/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  styleUrls: ['./recipe-edit.component.css'],
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
+  isEdit = false;
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = +params['id'];
+      this.isEdit = Boolean(id);
+    });
   }
-
 }

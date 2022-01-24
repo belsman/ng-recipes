57c13e48c0f41ddfa52b4afca01eadc20ed44e19
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-25 00:24:28 +0100

    Add routes for new and edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './features/recipes/recipe-edit/recipe-edit.component';
 
 import { RecipesComponent } from './features/recipes/recipes/recipes.component';
 import { SelectRecipeComponent } from './features/recipes/select-recipe/select-recipe.component';
@@ -13,7 +14,9 @@ const appRoutes: Routes = [
     component: RecipesComponent,
     children: [
       { path: '', component: SelectRecipeComponent },
+      { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
     ],
   },
   { path: 'shopping-list', component: ShoppingListComponent },

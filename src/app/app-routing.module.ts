d7d336b9a0794d37c1a93d902a3be5ff45cbc000
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-24 22:27:17 +0100

    Add nested routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
 
 import { RecipesComponent } from './features/recipes/recipes/recipes.component';
+import { SelectRecipeComponent } from './features/recipes/select-recipe/select-recipe.component';
 import { ShoppingListComponent } from './features/shopping/shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      { path: '', component: SelectRecipeComponent },
+      { path: ':id', component: RecipeDetailComponent },
+    ],
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 

b58687d49b393e1e3252234fc44c2329c6055985
Bello Babakolo <fleetbeekay@gmail.com>
2022-02-16 09:51:18 +0100

    Navigate to new route on recipe creation

--- src/app/features/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/features/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -15,7 +15,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -33,8 +34,10 @@ export class RecipeEditComponent implements OnInit {
         ...this.recipeForm.value,
         id: this.id,
       });
+      this.router.navigate(['recipes', this.id]);
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
+      this.router.navigate(['recipes']);
     }
   }
 
